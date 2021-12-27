b940b48af138fc74fd3e0df6e5cd9f2b5d8644c1
emirMaya <emirmayaok@gmail.com>
2021-12-27 10:49:48 -0300

    first commit

---
+++ javascript/main.js
@@ -0,0 +1,36 @@
+// var fruta tiene almacenado una pera
+// var fruta = 'Pera';
+// // la var num almacena el numero 20
+// const num = 20;
+
+// let num2 = 30;
+
+// let dni = '40678797';
+
+// let suma = num + num2 + dni;
+// let suma = num * num2;
+
+// console.log(suma);
+
+
+// let estoyDormido = 'false';
+
+// Obtener un dato, perdirlo
+let nombre = prompt('Escriba su nombre');
+
+// imprimirlo
+console.log('Hola ' +nombre + ', a continuación te vamos a pedir un número.');
+
+let num1 = prompt ('Escribe un número ' + nombre + '. Luego le sumaremos a ese número 20.');
+
+num1 = parseInt(num1);
+
+let num2 = 20;
+
+var tot = num1 + num2;
+
+console.log ( tot)
+// console.log(`${fruta} es muy rica`);
+
+// mostrar un alerta
+alert('¡El resultado es ' + tot +'!');
